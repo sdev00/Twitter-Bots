fdd01a15bbe679d754323be7ad960fcda97e09aa
Joshua Goldberg <joshg4096@gmail.com>
2020-11-08 10:29:08 -0500

    Add midterm 2018 and dataset download script

---
+++ datasets/download_datasets.sh
@@ -0,0 +1,7 @@
+wget https://botometer.osome.iu.edu/bot-repository/datasets/cresci-rtbust-2019/cresci-rtbust-2019.tar.gz
+
+tar xf cresci-rtbust-2019.tar.gz
+
+wget https://botometer.osome.iu.edu/bot-repository/datasets/midterm-2018/midterm-2018.tar.gz
+
+tar xf midterm-2018.tar.gz
